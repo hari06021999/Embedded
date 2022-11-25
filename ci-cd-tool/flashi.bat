1e900c0a3ab3913e1c1f36494b9280bdd2fa4b45
hari_08 <97215411+hari06021999@users.noreply.github.com>
2022-11-25 17:54:10 +0530

    Update flashi.bat

--- ci-cd-tool/flashi.bat
+++ ci-cd-tool/flashi.bat
@@ -8,6 +8,6 @@ if not [%1]==[] set "build_type=%1"
 
 set "ws_root=C:\Users\harip\STM32CubeIDE\workspace_1.10.1\LED"
 set "sn=066EFF485688494867113960"
-set "image_file=%ws_root%\%build_type%\LED.bin"
+set "image_file=%ws_root%\%build_type%\LED.elf"
 
 "%ws_root%\CICDTool\flash.bat" %sn% "%image_file%"
