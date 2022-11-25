6bd21d93305a70c3717eec2eae4211579cd9cadd
hari_08 <97215411+hari06021999@users.noreply.github.com>
2022-11-25 17:44:09 +0530

    Create flashi.bat

---
+++ ci-cd-tool/flashi.bat
@@ -0,0 +1,13 @@
+rem @echo off
+set "usage=usage: flashi [{Debug|Release}]"
+
+setlocal
+
+set "build_type=Debug"
+if not [%1]==[] set "build_type=%1"
+
+set "ws_root=C:\Users\harip\STM32CubeIDE\workspace_1.10.1\LED"
+set "sn=066EFF485688494867113960"
+set "image_file=%ws_root%\%build_type%\LED.bin"
+
+"%ws_root%\CICDTool\flash.bat" %sn% "%image_file%"
