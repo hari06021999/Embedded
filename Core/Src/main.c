289f00072b30ef6ee52aa85ba87b5da809127f61
hari_08 <97215411+hari06021999@users.noreply.github.com>
2022-11-26 18:56:48 +0530

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -98,11 +98,11 @@ int main(void)
   {
     /* USER CODE END WHILE */
 	
-		 HA_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+		 HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
 		 HAL_Delay(1000);
 	
 	 
-		 HA_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+		 HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 	         HAL_Delay(1000);	         
 	 
 
